1f6e13686c0841ff5d78eb0e591dd95b745f7d17
AnshumanSharma05 <sharma.anshuman97@gmail.com>
2020-12-01 09:55:37 +0530

    Introduction to Python

---
+++ io.py
@@ -0,0 +1,16 @@
+"""Imagine that you are in a desert and all of a sudden, a space shuttle lands 
+in front of you. An alien walks out of the space shuttle and greets you. 
+Write a Python program to welcome this friendly alien to our planet - Earth.  
+Get the name of the alien from the user and display the welcome message as 
+given in the sample output.
+"""
+"""Sample Input:
+
+Enter the name:Naoto
+
+Sample Output:
+
+Hello Naoto! Welcome to our planet Earth.
+"""
+name=input("Enter the name")
+print("Hello "+name+"! Welcome to out planet Earth.")
